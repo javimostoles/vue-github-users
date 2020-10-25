1e533cfe3dbe3b085e046efd663232331ec52329
Javier Pérez <javier.perezdepedro@gexterno.es>
2020-10-25 13:53:57 +0100

    Add login template

--- vue-github-users/src/App.vue
+++ vue-github-users/src/App.vue
@@ -2,19 +2,41 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/login">Login</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  background: linear-gradient(to bottom, #68EACC 0%, #497BE8 100%);
+  min-height: 100vh;
+  display: flex;
+  font-weight: 400;
+}
+
+body,
+html {
+  width: 100%;
+  height: 100%;
+}
+
 #app {
+  position: relative;
+  width: 100%;
+  height: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
 }
 
 #nav {
@@ -29,4 +51,5 @@
     }
   }
 }
+
 </style>
